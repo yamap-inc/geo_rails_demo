4efa05df052dc705ac9ee58627364441505ec0af
Armin Kirchner <post.armin@gmail.com>
2023-02-16 16:49:39 +0900

    Query and convert municipality data to vector tile
    Vector tiles can be created with a PostGIS query.
    
    First, The geometry data needs to be limited for inclusion into the
    vector tiles. Only areas that intersect the tile boundary are
    considered. Overlapping zones are clipped.
    
    The scoped data is then converted to the Mapbox Vector Tile binary
    format.

--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -2,4 +2,12 @@
 
 class Municipality < ApplicationRecord
   has_many :municipality_areas, dependent: :destroy
+
+  def self.as_mvt_for(zoom:, x:, y:)
+    relation = joins(:municipality_areas)
+                 .merge(MunicipalityArea.as_mvt_geom_for(zoom:, x:, y:))
+                 .select(:id, :name)
+
+    all.unscoped.from(relation, :mvtgeom).select('ST_AsMVT(mvtgeom.*) mvt').take.mvt
+  end
 end
--- app/models/municipality_area.rb
+++ app/models/municipality_area.rb
@@ -2,4 +2,17 @@
 
 class MunicipalityArea < ApplicationRecord
   belongs_to :municipality
+
+  def self.for_map_tile(zoom:, x:, y:)
+    where('area && ST_TileEnvelope(:zoom, :x, :y)', zoom:, x:, y:)
+  end
+
+  def self.as_mvt_geom_for(zoom:, x:, y:)
+    sql = sanitize_sql_array([
+            "ST_AsMVTGeom(area, ST_TileEnvelope(:zoom, :x, :y)) AS geom",
+            zoom:, x:, y:,
+          ])
+
+    for_map_tile(zoom:, x:, y:).select(sql)
+  end
 end
